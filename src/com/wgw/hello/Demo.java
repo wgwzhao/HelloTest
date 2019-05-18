f94a5f11d569a85324b0ed1854b5d4486c569d58
415811958 <415811958@qq.com>
2019-05-18 10:11:18 +0800

    test

---
+++ src/com/wgw/hello/Demo.java
@@ -0,0 +1,25 @@
+package com.wgw.hello;
+
+import java.util.HashMap;
+
+public class Demo {
+
+    public static void main(String[] args){
+        Person p = new Person("wgw",19);
+        System.out.println(p.getAge());
+
+        System.out.println(p);
+
+
+
+
+
+        //注释
+        /*
+        this is my helloworld
+         */
+
+        HashMap<Object, Object> map = new HashMap<>();
+
+    }
+}
